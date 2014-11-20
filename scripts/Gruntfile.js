3590cbe5da1e3501fb0f39f2bb4ca747eb5d798d
Christopher A. Moore <chris.a.moore@gmail.com>
2014-11-20 10:43:10 -0800

    update grunt and ignore

--- scripts/Gruntfile.js
+++ scripts/Gruntfile.js
@@ -4,21 +4,25 @@ var chalk = require('chalk');
 
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-aglio');
+    grunt.loadNpmTasks('grunt-dredd');
+    grunt.loadNpmTasks('grunt-apimock');
 
-    /**
-     * Import SQL file into DB
-     *
-     *  @args
-     *      --file="string"
-     *      --db="string"
-     *      --username="string"
-     *      --password="string"
-     *
-     *  @usage
-     *      grunt import --file="/vagrant/my-box-config/sql/dump.sql" --db="mydb" --username="root" --password="root"
-     */
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * Import SQL file into DB
+         *
+         *  @args
+         *      --file="string"
+         *      --db="string"
+         *      --username="string"
+         *      --password="string"
+         *
+         *  @usage
+         *      grunt import --file="/vagrant/my-box-config/sql/dump.sql" --db="mydb" --username="root" --password="root"
+         */
         shell: {
             import: {
                 command: function () {
@@ -28,10 +32,25 @@ module.exports = function (grunt) {
                     callback: log
                 }
             }
-        }
+        },
+        /**
+         * Concat Aglio
+         */
+        aglio: {
+            your_target:{
+              files:{
+                "./api.html": ["./resources/users.md", "./resources/derp.md"]
+              },
+              options: {
+                theme: "slate",
+                separator: "\n"
+              }
+            }
+          }
     });
 
     grunt.registerTask('import', ['shell:import']);
+    grunt.registerTask('start:aglio', ['aglio']);
 
     grunt.registerTask('default', function () {
         grunt.log.write(chalk.cyan.bold('Hi I\'m the default task'));
